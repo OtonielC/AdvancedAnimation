99a840cab5868c66bfec430ece68410f92413ca5
OtonielC <800494@seq.org>
2020-08-28 10:15:54 -0700

    Advanced Animation
    First Commit

---
+++ Advanced Animation/End Of Year Project/sketch.js
@@ -0,0 +1,11 @@
+//Otoniel Carreon
+//End of Year Project
+var button = document.createElement("button");
+button.innerHTML = "Say Hi";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  alert("did something");
+});
